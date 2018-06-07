7bc12ed1906e8433c45ba8ae8bb1b2277324a68f
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2018-06-07 16:04:17 -0400

    update migrations to pin last ran migration

--- fec/home/migrations/0001_initial.py
+++ fec/home/migrations/0001_initial.py
@@ -7,17 +7,16 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0040_page_draft_title'),
+        ('wagtailcore', '0032_add_bulk_delete_page_permission'),
         ('wagtailadmin', '0001_create_admin_access_permissions'),
         ('wagtaildocs','0007_merge'),
         ('wagtailembeds','0003_capitalizeverbose'),
         ('wagtailforms','0003_capitalizeverbose'),
-        ('wagtailimages','0019_delete_filter'),
+        ('wagtailimages','0017_reduce_focal_point_key_max_length'),
         ('wagtailredirects','0005_capitalizeverbose'),
         ('wagtailsearch','0003_remove_editors_pick'),
         ('wagtailsearchpromotions','0002_capitalizeverbose'),
-        ('wagtailusers','0006_userprofile_prefered_language'),
-
+        ('wagtailusers','0005_make_related_name_wagtail_specific'),
     ]
 
     operations = [
