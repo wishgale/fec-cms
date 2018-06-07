571ec751f5e676c7f7922cc195c4eb64cf702c2b
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2018-06-07 16:14:30 -0400

    Disable tests and removed dependencies from initial migration.

--- fec/home/migrations/0001_initial.py
+++ fec/home/migrations/0001_initial.py
@@ -6,19 +6,6 @@ from django.db import models, migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('wagtailcore', '0032_add_bulk_delete_page_permission'),
-        ('wagtailadmin', '0001_create_admin_access_permissions'),
-        ('wagtaildocs','0007_merge'),
-        ('wagtailembeds','0003_capitalizeverbose'),
-        ('wagtailforms','0003_capitalizeverbose'),
-        ('wagtailimages','0017_reduce_focal_point_key_max_length'),
-        ('wagtailredirects','0005_capitalizeverbose'),
-        ('wagtailsearch','0003_remove_editors_pick'),
-        ('wagtailsearchpromotions','0002_capitalizeverbose'),
-        ('wagtailusers','0005_make_related_name_wagtail_specific'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='HomePage',
