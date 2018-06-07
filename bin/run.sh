fb03ca7e9910f7d9c9ffdfa7eb5569f329a40050
Pat Phongsvirajati <pphongsvirajati@fec.gov>
2018-06-07 15:41:03 -0400

    initial push with changes to fix django test

--- bin/run.sh
+++ bin/run.sh
@@ -10,7 +10,17 @@ set -o pipefail
 
 cd fec
 # Run migrations
-./manage.py makemigrations
+./manage.py makemigrations 
+./manage.py migrate wagtailadmin --noinput
+./manage.py migrate wagtailcore --noinput
+./manage.py migrate wagtaildocs --noinput
+./manage.py migrate wagtailembeds --noinput
+./manage.py migrate wagtailforms --noinput
+./manage.py migrate wagtailimages --noinput
+./manage.py migrate wagtailredirects --noinput
+./manage.py migrate wagtailsearch --noinput
+./manage.py migrate wagtailsearchpromotions --noinput
+./manage.py migrate wagtailusers --noinput
 ./manage.py migrate --noinput
 
 # Run application
