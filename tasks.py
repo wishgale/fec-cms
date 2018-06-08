3c414ee538a5114a92000246e0a3b192b6d7fea2
Patcharee Phongsvirajati <pphongsvirajati@fec.gov>
2018-06-08 16:05:28 -0400

    adding task to deploy task for this branch to stage

--- tasks.py
+++ tasks.py
@@ -74,7 +74,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     #('stage', lambda _, branch: branch.startswith('release')),
-    ('stage', lambda _, branch: branch == 'feature/migration-updates'),
+    ('stage', lambda _, branch: branch == 'feature/fix-migration-tests'),
     ('dev', lambda _, branch: branch == 'develop'),
     # Uncomment below and adjust branch name to deploy desired feature branch to the feature space
     # ('feature', lambda _, branch: branch == 'feature/INSERT_BRANCH_NAME'),
